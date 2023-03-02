e3e40c2d7565ba66c7fe5ffd8aea9a43a23c2995
BhavyaBh289 <shahbhavyasp@gmail.com>
2023-03-02 11:20:56 +0530

    630cc

---
+++ register.php
@@ -0,0 +1,121 @@
+<?php
+
+include 'config.php';
+
+if(isset($_POST['submit'])){
+
+   $name = mysqli_real_escape_string($conn, $_POST['name']);
+   $email = mysqli_real_escape_string($conn, $_POST['email']);
+   $pass = mysqli_real_escape_string($conn, md5($_POST['password']));
+   $cpass = mysqli_real_escape_string($conn, md5($_POST['cpassword']));
+   $user_type = $_POST['user_type'];
+
+   $select_users = mysqli_query($conn, "SELECT * FROM `users` WHERE email = '$email' AND password = '$pass'") or die('query failed');
+
+   if(mysqli_num_rows($select_users) > 0){
+      $message[] = 'user already exist!';
+   }else{
+      if($pass != $cpass){
+         $message[] = 'confirm password not matched!';
+      }else{
+         mysqli_query($conn, "INSERT INTO `users`(name, email, password, user_type) VALUES('$name', '$email', '$pass', '$user_type')") or die('query failed');
+         $message[] = 'registered successfully!';
+         header('location:login.php');
+      }
+   }
+
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <link rel="stylesheet" href="style_login.css">
+   <script src="https://cdnjs.cloudflare.com/ajax/libs/prefixfree/1.0.7/prefixfree.min.js"></script>
+   <title>register</title>
+
+   <!-- font awesome cdn link  -->
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
+
+   <!-- custom css file link  -->
+   <link rel="stylesheet" href="css/style.css">
+   <link rel="stylesheet" href="style_login.css">
+<style>
+.container:hover .top:before, .container:hover .top:after, .container:hover .bottom:before, .container:hover .bottom:after, .container:active .top:before, .container:active .top:after, .container:active .bottom:before, .container:active .bottom:after {
+  margin-left: 200px;
+  transform-origin: -200px 50%;
+  transition-delay: 0s;
+}
+   </style>
+
+</head>
+<body>
+
+
+
+<?php
+if(isset($message)){
+   foreach($message as $message){
+      echo '
+      <div class="message">
+         <span>'.$message.'</span>
+         <i class="fas fa-times" onclick="this.parentElement.remove();"></i>
+      </div>
+      ';
+   }
+}
+?>
+
+
+<form action="" method="post">
+<div class="container" onclick="onclick">
+  <div class="top"></div>
+  <div class="bottom"></div>
+  <div class="center">
+    <h2>Please Sign In</h2>
+    <input type="text" name="name" placeholder="enter your name" required class="box">
+    <input type="email" name="email" placeholder="enter your email" required class="box">
+    <input type="password" name="password" placeholder="enter your password" required class="box">
+    <input type="password" name="cpassword" placeholder="confirm your password" required class="box">
+    <select name="user_type" class="box">
+         <option value="user">user</option>
+         <option value="admin">admin</option>
+      </select>
+      <input type="submit" name="submit" value="register now" class="btn">
+      <p>already have an account? <a href="login.php">login now</a></p>
+
+    <h2>&nbsp;</h2>
+  </div>
+</div>
+   </form>
+
+
+<script src='https://codepen.io/banik/pen/ReNNrO/3f837b2f0085b5125112fc455941ea94.js'></script>
+
+<!-- 
+
+   
+<div class="form-container">
+
+   <form action="" method="post">
+      <h3>register now</h3>
+      <input type="text" name="name" placeholder="enter your name" required class="box">
+      <input type="email" name="email" placeholder="enter your email" required class="box">
+      <input type="password" name="password" placeholder="enter your password" required class="box">
+      <input type="password" name="cpassword" placeholder="confirm your password" required class="box">
+      <select name="user_type" class="box">
+         <option value="user">user</option>
+         <option value="admin">admin</option>
+      </select>
+      <input type="submit" name="submit" value="register now" class="btn">
+      <p>already have an account? <a href="login.php">login now</a></p>
+   </form>
+
+</div> -->
+
+</body>
+</html>
